c31fce999a2d06a7d0480f089b71bbb7cf557b9f
Sergio Brenes <brenes93@hotmail.es>
2025-03-20 22:23:08 +0100

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -8,12 +8,6 @@ npm run build
 cd ../..
 
 pipenv install
-pipenv run reset_db
-pipenv run flask insert-test-users 5
-pipenv run flask insert_data_catfood
-pipenv run flask insert_data_dogfood
-pipenv run flask insert_data_exoticfood
-pipenv run flask insert_data_accessories
-pipenv run flask insert_data_pet
+
 pipenv run migrate
 pipenv run upgrade
